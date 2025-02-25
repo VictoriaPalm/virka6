d2d8422cb40598b3cefda3285a946f2c1b1a965a
VictoriaPalm <jlh252@alumni.ku.dk>
2025-02-25 15:36:47 +0100

    Test

--- GalagaTests/TestPlayer.cs
+++ GalagaTests/TestPlayer.cs
@@ -5,25 +5,65 @@ using Galaga;
 using System.Numerics;
 using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
+using System.Linq;
 
 public class TestsPlayer {
-    public Player testPlayer;
-    public Player testPlayer2;
+    private Player testPlayer = null!;
+    private Player testPlayerAtRightBoundary = null!;
 
     [SetUp]
     public void Setup() {
         testPlayer = new Player(
             new DynamicShape(new Vector2(0.45f, 0.1f), new Vector2(0.1f, 0.1f)),
-            new Image("GalagaTests.Images.Player.png"));
-        testPlayer2 = new Player(
+            new NoImage()
+        );
+
+        testPlayerAtRightBoundary = new Player(
             new DynamicShape(new Vector2(0.99f, 0.1f), new Vector2(0.1f, 0.1f)),
-            new NoImage());
+            new NoImage()
+        );
     }
 
     [Test]
-    public void Test1() {
-        Assert.AreEqual(1, 1);
+    public void Test_PlayerMovesLeft() {
+        testPlayer.SetMoveLeft(true);
+        testPlayer.Move();
+        Assert.That(testPlayer.GetPosition().X, Is.LessThan(0.45f));
     }
 
-    // TODO: Your tests
+    [Test]
+    public void Test_PlayerMovesRight() {
+        testPlayer.SetMoveRight(true);
+        testPlayer.Move();
+        Assert.That(testPlayer.GetPosition().X, Is.GreaterThan(0.45f));
+    }
+
+    [Test]
+    public void Test_PlayerStopsMoving() {
+        testPlayer.SetMoveLeft(false);
+        testPlayer.SetMoveRight(false);
+        testPlayer.Move();
+        Assert.That(testPlayer.GetPosition().X, Is.EqualTo(0.45f));
+    }
+
+    [Test]
+    public void Test_PlayerDoesNotMovePastLeftBoundary() {
+        testPlayer.Shape.Position = new Vector2(0.0f, testPlayer.Shape.Position.Y);
+        testPlayer.SetMoveLeft(true);
+        testPlayer.Move();
+        Assert.That(testPlayer.GetPosition().X, Is.EqualTo(0.0f));
+    }
+
+    [Test]
+    public void Test_PlayerDoesNotMovePastRightBoundary() {
+        testPlayerAtRightBoundary.SetMoveRight(true);
+        testPlayerAtRightBoundary.Move();
+        Assert.That(testPlayerAtRightBoundary.GetPosition().X, Is.LessThanOrEqualTo(1.0f));
+    }
+
+    [Test]
+    public void Test_PlayerPositionMethodWorks() {
+        Vector2 position = testPlayer.GetPosition();
+        Assert.That(position, Is.EqualTo(new Vector2(0.45f, 0.1f)));
+    }
 }
